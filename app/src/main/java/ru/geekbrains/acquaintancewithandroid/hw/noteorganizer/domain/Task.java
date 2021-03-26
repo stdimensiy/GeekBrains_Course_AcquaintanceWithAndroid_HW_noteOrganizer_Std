baceab50b990c4816f5cf286df4f94b9b7ccb417
StDimensiy <stdimensiy@yandex.ru>
2021-03-26 14:55:53 +0300

    -  в классе Note изменен тип значения даты создания заметки на Long -  в классе Task соответственно тип Calendar заменен на Long -  в классе задачи объявлено поле (флаг) отвечающее за общее состояние задачи (отмечена или нет) -  в классе задачи объявлено поле (флаг) содержащее признак наличия в задаче не только наименования, но и какого-то контента.

--- app/src/main/java/ru/geekbrains/acquaintancewithandroid/hw/noteorganizer/domain/Task.java
+++ app/src/main/java/ru/geekbrains/acquaintancewithandroid/hw/noteorganizer/domain/Task.java
@@ -3,13 +3,30 @@ package ru.geekbrains.acquaintancewithandroid.hw.noteorganizer.domain;
 import java.util.Calendar;
 
 public class Task extends Note {
-    private Calendar alarmDate;        //дата и время напоминания
+    private long alarmDate;        // дата и время напоминания
+    private boolean isChecked;     // состояние флажка задачи (отмечено или нет)
+    private boolean isHaveContent; // признак наличия в задаче не только наименования но и содержания.
 
-    public Calendar getAlarmDate() {
+    public long getAlarmDate() {
         return alarmDate;
     }
-
-    public void setAlarmDate(Calendar alarmDate) {
+    public void setAlarmDate(long alarmDate) {
         this.alarmDate = alarmDate;
+    }
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public void setChecked(boolean checked) {
+        isChecked = checked;
+    }
+
+    public boolean isHaveContent() {
+        return isHaveContent;
+    }
+
+    public void setHaveContent(boolean haveContent) {
+        isHaveContent = haveContent;
     }
 }
